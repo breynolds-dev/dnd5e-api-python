6b5630dc5f40b024f10fcfcf65da119868f96151
arturusfury <bri.reynolds@gmail.com>
2017-06-12 19:27:13 -0500

    added CORS support with flask-cors to prevent any issues from get requests

--- dnd5eApi/__init__.py
+++ dnd5eApi/__init__.py
@@ -4,18 +4,26 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={"*": {"origins": "*"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 api = Api(app, prefix="/v1")
 
 import dnd5eApi.routes.endpoints
-
+#
+# db.reflect()
+# db.drop_all()
+#
 # db.create_all()
 # db.session.commit()
 #
